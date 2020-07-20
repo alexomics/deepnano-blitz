d675f9ee7a5703460dbcdcdeba1ff721fe1c39d3
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-07-20 23:43:43 +0100

    install ssl

--- .github/workflows/install_rust.sh
+++ .github/workflows/install_rust.sh
@@ -2,3 +2,8 @@ curl https://sh.rustup.rs -sSfo install-rust.sh
 sh install-rust.sh -y
 source $HOME/.cargo/env
 rustup default nightly
+
+if [ "$(uname)" == "Darwin" ]; then
+    brew install openssl@1.1
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sudo apt-get install pkg-config libssl-dev
